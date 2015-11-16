d1ffb3a02f61bbbb0b28bbde0fa41f809121c019
Kemal C <ddeveloperr@gmail.com>
2015-11-16 10:26:40 +0100

    Start projecta sa Yeoman.io; Start sa index i main.html osnovnom strukturom

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatAngulatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chatAngulatorApp
+ */
+angular.module('chatAngulatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
