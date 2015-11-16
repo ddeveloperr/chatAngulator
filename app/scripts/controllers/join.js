91c3d1b9d8c05d730b4ee2fffc1c267cb52fb75b
Kemal C <ddeveloperr@gmail.com>
2015-11-16 10:46:52 +0100

    Inicijalizacija controllers/join.js

---
+++ app/scripts/controllers/join.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatAngulatorApp.controller:JoinCtrl
+ * @description
+ * # JoinCtrl
+ * Controller of the chatAngulatorApp
+ */
+angular.module('chatAngulatorApp')
+  .controller('JoinCtrl', function ($scope) {
+    console.log(JoinCtrl Initialized....);
+  });
